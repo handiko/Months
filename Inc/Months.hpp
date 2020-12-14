3913adeac710f9a9582496c0119fac5bab7d551f
handiko <handikogesang@gmail.com>
2020-12-14 14:03:08 +0700

    first commit

---
+++ Inc/Months.hpp
@@ -0,0 +1,24 @@
+/*
+ * Months.hpp
+ *
+ *  Created on: Dec 14, 2020
+ *      Author: handiko
+ */
+
+#ifndef MONTHS_HPP_
+#define MONTHS_HPP_
+
+#define JANUARY 1
+#define FEBRUARY 2
+#define MARCH 3
+#define APRIL 4
+#define MAY 5
+#define JUNE 6
+#define JULY 7
+#define AUGUST 8
+#define SEPTEMBER 9
+#define OCTOBER 10
+#define NOVEMBER 11
+#define DECEMBER 12
+
+#endif /* MONTHS_HPP_ */
